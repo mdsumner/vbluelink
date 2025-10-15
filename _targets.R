f3645d96dddcaf950773c0fc68b00e95d879274b
Michael Sumner <mdsumner@gmail.com>
2025-10-15 03:55:56 +0000

    do it all

--- _targets.R
+++ _targets.R
@@ -17,8 +17,8 @@ targets <-tar_map(
   values = tidyr::expand_grid(
        data_source = c("atm_flux_diag", "ice_force", "ocean_eta_t", "ocean_force",
                   "ocean_mld", "ocean_salt", "ocean_temp", "ocean_tx_trans_int_z",
-                  "ocean_ty_trans_int_z", "ocean_u", "ocean_v", "ocean_w")[1:3],
-              time_chunk = c("annual", "daily", "month")[1]),
+                  "ocean_ty_trans_int_z", "ocean_u", "ocean_v", "ocean_w"),
+              time_chunk = c("annual", "daily", "month")),
 
   tar_target(regexp, create_regexp(time_chunk, data_source)),
   tar_target(files, bran_files("/g/data/gb6/BRAN/BRAN2023") |> dplyr::filter(str_detect(url, regexp))),
